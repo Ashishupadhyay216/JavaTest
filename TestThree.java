70f2cd9de00e9ea9941ea834f9e2bc9a9569be43
ashishupadhyay216 <ashishupadhyay216@gmail.com>
2025-05-27 16:57:36 +0530

    My program

---
+++ TestThree.java
@@ -0,0 +1,6 @@
+public class TestThree {
+    public static void main(String[] args) {
+        System.out.println("My commit and push is ready now");
+    }
+    
+}
---
+++ TestTwo.java
@@ -0,0 +1,10 @@
+class doo{
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Worl");
+    }
+    }
+    
+    
